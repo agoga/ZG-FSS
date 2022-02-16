cb7bf4b191f118d050ba32c8620e534b9b1a7319
Michael Kovtun <makovtun@ucdavis.edu>
2022-02-15 16:32:11 -0800

    updated localization_farm in git, added test and local test versions

--- localization_farm_test_local.py
+++ localization_farm_test_local.py
@@ -8,35 +8,38 @@ from scipy.optimize import curve_fit
 from scipy import stats
 from numpy import linalg as la
 
-def save(par, return_value, avg, name, time):
+import multiprocessing as mp
+import itertools
+import cProfile
+
+def save(par, return_value, avg, name):
 	now = datetime.datetime.now()
 	f = open(str(name),"a")
-	line = str(now)+" "+str(par)+" "+str(return_value)+" "+str(avg)+" "+str(time)+"s\n"
+	line = str(now)+" "+str(par)+" "+str(return_value)+" "+str(avg)+"\n"
 	f.write(line)
 
 def Create_Transfer_Matrix(coupling_matrix_down, W, t_low, c, L, E, dim):
 	# P(t)= c*delta(t-t_h) + (1-c)*delta(t-t_l)
+	# fraction=c in Z group
 	# Dont use dim==2!
-
+	# W does nothing
 	N = L * L
 	if dim == 3:
-		# generate a diagonal of 1s
-		W_strip = W*(np.random.rand(N)-0.5)
-		#make <\eps_i>=0 exactly
-		#W_strip = W_strip - np.mean(W_strip)
+		W_strip = W * (np.random.rand(N) - 0.5)
+		# make <\eps_i>=0 exactly
+		# W_strip = W_strip - np.mean(W_strip)
 
 		# coupling_up is used to form the matrix that couples the nth strip or bar to the n+1th strip or bar
 		coupling_up = []
 		for i in range(N):
 			# c determines the connectivity (fraction of good links) on the lattice
 			if np.random.random() < c:
-				coupling_up.append(1.0) #t_hi
+				coupling_up.append(1.0)  # t_hi
 			else:
 				coupling_up.append(t_low)
 
 		coupling_matrix_up = np.diag(coupling_up)
 		coupling_up_inv = np.linalg.inv(coupling_matrix_up)
-
 		# generate the intra-strip hamiltonian
 		minilist = np.zeros(L)
 		minilist[1] = 1
@@ -247,126 +250,155 @@ def Create_Transfer_Matrix_Chase(coupling_matrix_down, W, t_low, fraction, size,
 
 	return [transfer_matrix, coupling_matrix_up]
 
+def doCalc(eps,min_Lz,L,W,t_low,c,E,dim):
+	#eps: desired error
+	#min_Lz: At least this many slices will be computed, no matter what
+	#L, V, W: size, potential, diagonal disorder
+	
+	#P(t)= c*delta(t-t_hi) + (1-c)*delta(t-t_low)
+	#t_hi is fixed at 1
+	#c: driving parameter. should be 0<c<1
+	#t_low: sets low off diagonal disorder value. Needs to be low enough to be in conducting regime
 
-def doCalc(eps, min_Lz, L, W, t_low, c, E, dim):
-	# eps: desired error
-	# min_Lz: At least this many slices will be computed, no matter what
-	# L, V, W: size, potential, diagonal disorder
+	#housekeeping
 
-	# P(t)= c*delta(t-t_hi) + (1-c)*delta(t-t_low)
-	# t_hi is fixed at 1
-	# c: driving parameter. should be 0<c<1
-	# t_low: sets low off diagonal disorder value. Needs to be low enough to be in conducting regime
-
-	# housekeeping
-	np.set_printoptions(linewidth=200)
-	if dim == 2:
-		N = L
+	if dim==2:
+		N=L
 	else:
-		N = L * L
+		N=L*L
+	
+	
+	#Performs the actual localization length and conductance calculations
+	eps_N=100000000000
+	Lz=0 #running length
+	n_i=5 #number of steps between orthogonalizations.
+	n_i_min=5 #Set n_i_min=n_i if you want to force n_i
+	Nr=1000 #number of T matrices to generate Q0 with
 
-	# Performs the actual localization length and conductance calculations
-	eps_N = 100000000000
-	Lz = 0  # running length
-	n_i = 5  # number of steps between orthogonalizations.
-	n_i_min = 5  # Set n_i_min=n_i if you want to force n_i
-	Nr = 1000  # number of T matrices to generate Q0 with
-
-	# generate first coupling matrix. If c=1, this is the identity matrix.
-	coupling_down = []
+	#generate first coupling matrix. If c=1, this is the identity matrix.
+	coupling_down=[]
 	for i in range(N):
-		if np.random.random() < c:
+		if np.random.random()<c:
 			coupling_down.append(1)
 		else:
 			coupling_down.append(t_low)
 	coupling_matrix_down = np.diag(coupling_down)
-	# Generate Q0
-	Q0 = np.random.rand(2 * N, N) - 0.5
+	#Generate Q0
+	Q0=np.random.rand(2*N,N)-0.5
 	Q0 = Q0.astype(np.float64)
 	Q0, r = np.linalg.qr(Q0)
-
+	
 	for i in range(Nr):
-		T, coupling_matrix_down = Create_Transfer_Matrix(coupling_matrix_down, W, t_low, c, L, E, dim)
-		Q0 = np.matmul(T, Q0)
-		if i % n_i == 0:
+		T, coupling_matrix_down =Create_Transfer_Matrix_Chase(coupling_matrix_down,W,t_low,c,L,E,dim)
+		Q0=np.matmul(T,Q0)
+		if i%n_i==0:
 			Q0, r = np.linalg.qr(Q0)
-
+	
 	Q0, r = np.linalg.qr(Q0)
-
-	d_a = np.zeros(N, dtype=np.float64)
-	e_a = np.zeros(N, dtype=np.float64)
-
-	lya = list()
-	glst = list()
-	Umat = Q0
-
-	while eps_N > eps or Lz < min_Lz:
-		M_ni, coupling_matrix_down = Create_Transfer_Matrix(coupling_matrix_down, W, t_low, c, L, E, dim)
-
+	
+	d_a=np.zeros(N,dtype=np.float64)
+	e_a=np.zeros(N,dtype=np.float64)
+	
+	lya=list()
+	glst=list()
+	Umat=Q0
+	timeit = 0
+	cnt =0
+	while eps_N>eps or Lz<min_Lz:
+		M_ni, coupling_matrix_down = Create_Transfer_Matrix_Chase(coupling_matrix_down,W,t_low,c,L,E,dim)
+		
 		for i in range(n_i):
-			T, coupling_matrix_down = Create_Transfer_Matrix(coupling_matrix_down, W, t_low, c, L, E, dim)
-			M_ni = np.matmul(M_ni, T)
-		Umat = np.matmul(M_ni, Umat)
-
+			T, coupling_matrix_down = Create_Transfer_Matrix_Chase(coupling_matrix_down,W,t_low,c,L,E,dim)
+			M_ni=np.matmul(M_ni,T)
+		Umat=np.matmul(M_ni,Umat)
 		Umat, r = np.linalg.qr(Umat)
 
 		w_a_norm = np.abs(np.diagonal(r))
-		d_a = d_a + np.log(w_a_norm)
-		e_a = e_a + np.square(np.log(w_a_norm))
+		d_a=d_a+np.log(w_a_norm)
+		e_a=e_a+np.square(np.log(w_a_norm))
+		
+		#D_i.append(1/n_i*np.log(w_a_norm))
 
-		# D_i.append(1/n_i*np.log(w_a_norm))
+		Lz=Lz+n_i
+		xi_a=d_a/Lz #these are the lyapunov exponents
+		nu_a=e_a/Lz
+		eps_a=np.sqrt(nu_a-xi_a**2)
+		
+		sum_xi=np.sum(xi_a)
 
-		Lz = Lz + n_i
-		xi_a = d_a / Lz  # these are the lyapunov exponents
-		nu_a = e_a / Lz
-		eps_a = np.sqrt(nu_a - xi_a ** 2)
-
-		sum_xi = np.sum(xi_a)
-
-		old_xi_a = xi_a[N - 1]
-		sm_pos = np.where(xi_a > 0, xi_a, np.inf).min()
+		old_xi_a = xi_a[N-1]
+		sm_pos = np.where(xi_a>0, xi_a, np.inf).min()
+		if old_xi_a !=sm_pos:
+			cnt += 1
 		lya.append(sm_pos)
-		glst.append(np.log(np.sum(1 / np.cosh(L * xi_a) ** 2)))
-		# eps_N = eps_a[N-1]
+		glst.append(np.log(np.sum(1/np.cosh(L*xi_a)**2)))
+		#eps_N = eps_a[np.where(xi_a==sm_pos)]
 
-		if len(lya) <= 1:
-			eps_N = 1.0  # avoid problems that occur if n_i is too big on the first go-through
+			
+		if len(lya)<=1:
+			eps_N = 1.0 #avoid problems that occur if n_i is too big on the first go-through
 		else:
-			eps_N = stats.sem(lya) / np.mean(lya)  # standard error
-		#if Lz % 200 == 0:
-		#		print("Lz: %d" % Lz)
-		#	print("Avg Smallest LE: %.7f" % np.mean(lya))
-		#	print("Total LEs: %d" % len(lya))
-		#	print("Std. Error: %.7f" % (stats.sem(lya) / np.mean(lya)))
-		#	print("T: %.5fs" % (time.time() - timeit))
+			eps_N = stats.sem(lya)/np.mean(lya) #standard error
+
+		monitor = True
+		if Lz % 2000 == 0 and monitor==True:
+			print("L: %d, c: %.3f, E: %.1f"%(L,c,E))
+			print("Lz: %d" % Lz)
+			print("Avg Smallest LE: %.7f" % np.mean(lya))
+			print("Std. Error: %.7f%%" % (stats.sem(lya) / np.mean(lya) * 100))
+			print("# of discrepencies: %d"%cnt)
+			print("======================================")
+			timeit = time.time()
 
 	smallestLEAvg = np.mean(lya)
+	
+	g=np.exp(np.mean(glst))
+	print("Calculation complete:")
+	print("L: %d, c: %.3f, E: %.1f" % (L, c, E))
+	print("Lz: %d" % Lz)
+	print("Avg Smallest LE: %.7f" % np.mean(lya))
+	print("Std. Error: %.7f%%" % (stats.sem(lya) / np.mean(lya) * 100))
+	print("Std. Dev: %.7f"%np.std(lya))
+	print("======================================")
+	return np.array([float(smallestLEAvg), np.std(lya) , g],dtype=object) #avg of LEs, standard error of mean, g
 
-	g = np.exp(np.mean(glst))
 
-	return np.array([float(smallestLEAvg), np.std(lya), g], dtype=object)
+def main():
+	filename = "offdiagtest.txt"
+	eps = 1.0
+	min_Lz = 10000
+	L = 6
+	W = 10
+	t_low = 0.3
+	c = 0.14
+	E = 10
+	dim = 3
+	avg = 1
 
+	crange = np.linspace(0.35,0.45, 11)
+	Lrange = np.arange(8, 14)
+	params=(eps,min_Lz,L,W,t_low,c,E,dim)
 
-### Calculate localization length
+	pool = mp.Pool(processes=6)
+	start = time.time()
+	results = pool.starmap(doCalc, ((params, ) * avg))
+	#results = doCalc(*params)
 
-eps=float(sys.argv[1])
-min_Lz=float(sys.argv[2])
-L=int(sys.argv[3])
-W=float(sys.argv[4])
-t_low=float(sys.argv[5])
-c=float(sys.argv[6])
-E=float(sys.argv[7])
-dim=int(sys.argv[8])
-avg=int(sys.argv[9])
-name=sys.argv[10]
+	end = time.time()
+	#print("g: %.7f"%ret[2])
+	#save(params, ret, avg, filename)
+	results = np.squeeze(results)
+	plt.hist(results[:,0], bins='auto')
+	plt.xlabel("Lyapunov Exp")
+	avgSmPosLE = np.average(results[:,0])
+	SESmPosLE = np.sqrt(np.sum(results[:,1]**2))/(avgSmPosLE * np.sqrt(avg))
+	#SESmPosLE = stats.sem(results[:,0])/avgSmPosLE
+	print("Average LE: %.7f" % avgSmPosLE)
+	print("Std Err. LE: %.7f" % SESmPosLE)
+	print(str(end-start) + " seconds elapsed.")
+	plt.show()
 
-start = time.time()
-params=(eps,min_Lz,L,W,t_low,c,E,dim)
-B = np.array([doCalc(*params) for x in range(avg)],dtype=object) #do the calculation and the averaging
-ret=np.array([np.mean(B[:,0]),np.sqrt(np.sum(B[:,1]**2)),np.mean(B[:,2])],dtype=object) #avg lypunov exp, avg g
-#print("Avg. LE: %.7f"%ret[0])
-#print("Std. Dev: %.7f"%ret[1])
-#print("g: %.7f"%ret[2])
-end = time.time()
-#print("Took "+str(end-start)+" seconds")
-save(params,ret,avg,name, end-start)
+if __name__=="__main__":
+	np.set_printoptions(linewidth=400)
+	cProfile.run('main()', sort='time')
+	#main()
